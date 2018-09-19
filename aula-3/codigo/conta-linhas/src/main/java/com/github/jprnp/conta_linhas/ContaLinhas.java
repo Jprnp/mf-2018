72e71e1665a6ce4d993de9055426358865807916
João Pedro Pinheiro <jprnp18@gmail.com>
2018-09-19 17:01:02 -0300

    Início contar linhas

---
+++ aula-3/codigo/conta-linhas/src/main/java/com/github/jprnp/conta_linhas/ContaLinhas.java
@@ -0,0 +1,21 @@
+package com.github.jprnp.conta_linhas;
+
+/**
+ * Classe principal da aplicação.
+ * @author jprnp João Pedro Pinheiro
+ */
+public final class ContaLinhas {
+    /**
+     * Construtor privado para adequação com o checkstyle.
+     */
+    private ContaLinhas() { }
+
+    /**
+     * Método de execução principal.
+     * @param args Argumentos passados ao programa
+     */
+    public static void main(final String[] args) {
+        // TODO Auto-generated method stub
+    }
+
+}
